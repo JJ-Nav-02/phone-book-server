377d3bc3d248a812aeca99c049d46f2f586fd998
Joanna de Guadalupe Jiménez Vázquez <jimenez6201@gmail.com>
2023-04-16 23:02:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-//const cors = require('cors')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
-//app.use(cors())
+app.use(cors())
 
 morgan.token('person', function (req, response) { 
   return `${JSON.stringify(req.body)}` })
